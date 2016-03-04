f1b728b7148d897ac0fab5b92b760ffabdbf3f3a
luismartinez24 <luismartinez242011@gmail.com>
2016-03-04 12:39:25 -0600

    feat: add django rest framework

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from quickstart.views import (
+                    UserViewSet, 
+                    GroupViewSet)
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'tutorial.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
